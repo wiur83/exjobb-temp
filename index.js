3c4df145d623e4cf75eae72c4ce7ba8b3aafa92e
Witold Urbas <aiur18@student.bth.se>
2020-03-20 20:19:57 +0100

    update

--- index.js
+++ index.js
@@ -7,12 +7,39 @@ const mongoose = require('mongoose');
 const authRoute = require("./routes/auth");
 const testRoute = require("./routes/test"); //Test
 
+//Config
 dotenv.config();
+app.use(express.urlencoded({ extended: false }));
 
 //connecting to db
 mongoose.connect( process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
     console.log("connected to DB")
 );
+
+//Index GET
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+//Login GET
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+//register GET
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+});
+
+//logout GET
+app.get("/logout", (req, res) => {
+    res.header("auth-token", "");
+});
+
+//Test GET
+app.get("/test", (req, res) => {
+    res.render("test.ejs", ({name: "pelle"}));
+});
 
 //middleware
 app.use(express.json());
